c1a6dc7f479b260c1fd17393ee916c5b9ecaf589
jemoran42 <38846574+jemoran42@users.noreply.github.com>
2018-04-30 11:03:23 -0500

    Update Rename_File.py

--- Rename_File.py
+++ Rename_File.py
@@ -13,23 +13,12 @@ def convert_csv_to_arrays(filename, old_index, new_index):
  
     return old_file, new_file
 	
-old_file,new_file = convert_csv_to_arrays('/home/tntech.edu/jemoran42/Caffe/Cars/cars_annos.csv', 7, 8)
+old_file,new_file = convert_csv_to_arrays('/path_to_dir/cars_annos.csv', 7, 8)
 
-path = '/home/tntech.edu/jemoran42/Caffe/Cars/car_ims'
+path = '/home/tntech.edu/yourusername/path_to_dir/car_ims'
 
 for i in range(len(old_file)):
 	for files in os.listdir(path):
 		if old_file[i] in files:
 			os.rename(os.path.join(path, old_file[i]), os.path.join(path, new_file[i]))
 			print old_file[i] + ' to ' + new_file[i]
-
-			
-# old_file,new_file = convert_csv_to_arrays('/home/tntech.edu/jemoran42/Caffe/Cars/cars_test_annos.csv', 0, 1)
-
-# path = '/home/tntech.edu/jemoran42/Caffe/Cars/cars_test'
-
-# for i in range(len(old_file)):
-	# for files in os.listdir(path):
-		# if old_file[i] in files:
-			# os.rename(os.path.join(path, old_file[i]), os.path.join(path, new_file[i]))
-			# print old_file[i] + ' to ' + new_file[i]
