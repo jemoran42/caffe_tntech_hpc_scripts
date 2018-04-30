b5f5831fb550b60fe0070a922a53c65f4d477a77
Jacob Moran <jmoran0405@gmail.com>
2018-04-29 21:43:50 -0500

    Adding exising file

---
+++ GoogLeNet_Cats_Dogs_Train.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=56000
+
+
+module load caffe
+hostname
+
+echo 'Training'
+caffe train --solver=/home/tntech.edu/jemoran42/Caffe/caffe/models/bvlc_googlenet/quick_solver.prototxt --weights=/home/tntech.edu/jemoran42/Caffe/caffe/models/bvlc_googlenet/bvlc_googlenet.caffemodel -gpu=0
+
+echo 'Done'
